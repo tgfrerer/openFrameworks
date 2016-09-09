d8de8f9327f01c335da97c1186d06af1b87b68de
Tim Gfrerer <tim@poniesandlight.co.uk>
2016-09-09 18:11:31 +0100

    add samplerbinding state to draw command info
    + tightly packed sampler binding state tells us which textures to bind,
      but is also used to calculate a hash for the descriptorset, so that we
      may cache descriptorsets.

--- libs/openFrameworks/vk/DrawCommand.cpp
+++ libs/openFrameworks/vk/DrawCommand.cpp
@@ -7,15 +7,16 @@ of::DrawCommand::DrawCommand( const DrawCommandInfo & dcs )
 	:mDrawCommandInfo( dcs ){
 	auto state = ( const_cast<DrawCommandInfo&>( mDrawCommandInfo ) );
 
-	// once the pipeline is inside a draw command we precalculate 
-	// but this doesn't make sense when the renderpass is part
-	// of pipeline code, too.
-
-	mPipelineHash = state.getPipeline().calculateHash();
-
 	// at this point we can find out how many descriptors 
 	// this draw operation will need, and we can store 
 	// them with the draw operation.
 
+	//! Todo: initialise Ubo blobs with default values, based on 
+	//  default values received from Shader. 
+	//
+	// Shader should provide us with values to initialise, because
+	// these values depend on the shader, and the shader knows the
+	// uniform variable types.
 }
+
 
--- libs/openFrameworks/vk/DrawCommand.h
+++ libs/openFrameworks/vk/DrawCommand.h
@@ -17,14 +17,46 @@ class RenderBatch;
 class DrawCommandInfo {
 	
 	friend class DrawCommand;
-							                        
-	std::map<std::string, std::vector<uint8_t>>       uboData;
-	std::map<std::string, std::vector<::vk::Sampler>> samplerData;
+	friend class RenderBatch;
+	
+	struct DescriptorSetData
+	{
+		// Sparse map from binding number to blob of UBO data
+		// Each vector serialises data values of all members within same UBO
+		std::map<uint32_t, std::vector<uint8_t>>       uboData;
+		
+		// map everything needed to bind an image
+		// to sparse binding number and binding array index
+		struct SamplerBindings
+		{
+			::vk::Sampler        sampler       = 0;
+			::vk::ImageView      imageView     = 0;
+			::vk::ImageLayout    imageLayout   = ::vk::ImageLayout::eUndefined;
+			uint32_t             bindingNumber = 0; // <-- may be sparse, may repeat (for arrays of images bound to the same binding), but must increase be monotonically (may only repeat or up over the series inside the samplerBindings vector).
+			uint64_t             arrayIndex    = 0;
+		};
+		
+		// Sparse list of bindings 
+		std::vector<SamplerBindings> samplerBindings; // must be tightly packed.
 
-	std::map<std::string, BufferObject * >          attributeData;
-	IndexBufferObject *                             indexData        = nullptr;
+		static_assert( (
+			+ sizeof( SamplerBindings::sampler )
+			+ sizeof( SamplerBindings::imageView )
+			+ sizeof( SamplerBindings::imageLayout )
+			+ sizeof( SamplerBindings::bindingNumber )
+			+ sizeof( SamplerBindings::arrayIndex )
+			) == sizeof( SamplerBindings ), "SamplerBindings layout is not tightly packed, but that's needed for hash calc." );
 
-	vk::GraphicsPipelineState                       pipeline;
+	};
+
+	// Bindings data, (vector index == set number) -- indices must not be sparse!
+	std::vector<DescriptorSetData> descriptorSetState;
+
+	// Map from attribute location to buffer of attribute data
+	std::map<uint32_t, BufferObject * >                attributeData;
+	IndexBufferObject *                                indexData = nullptr;
+
+	vk::GraphicsPipelineState                          pipeline;
 
 public:
 
@@ -37,6 +69,9 @@ public:
 		return pipeline;
 	}
 
+	const std::vector<DescriptorSetData>& getDescriptorSetState(){
+		return descriptorSetState;
+	}
 };
 
 // ----------------------------------------------------------------------
@@ -45,6 +80,7 @@ class DrawCommand
 {
 	// a draw command has everything needed to draw an object
 	const DrawCommandInfo mDrawCommandInfo;
+	// map from binding number to ubo data state
 
 	DrawCommand() = delete;
 	
