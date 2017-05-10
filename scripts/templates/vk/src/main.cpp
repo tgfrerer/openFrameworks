84e6096ef9e61908c2928441004d8c0c5f0c6dec
Tim Gfrerer <tim@poniesandlight.co.uk>
2017-05-10 17:32:03 +0100

    update vk template

--- scripts/templates/vk/src/main.cpp
+++ scripts/templates/vk/src/main.cpp
@@ -2,7 +2,8 @@
 #include "ofApp.h"
 
 int main(){
-	// Do basic initialisation (mostly setup timers, and randseed)
+	
+	// Basic initialisation (mostly setup timers, and randseed)
 	ofInit();
 
 	auto consoleLogger = new ofConsoleLoggerChannel();
@@ -10,18 +11,21 @@ int main(){
 
 	// Create a new window 
 	auto mainWindow = std::make_shared<ofAppGLFWWindow>();
-	//auto mainWindow = std::make_shared<ofAppVkNoWindow>();
+	
+	// use this instead to render using the image swapchain
+	// auto mainWindow = std::make_shared<ofAppVkNoWindow>();
 
 	// Store main window in mainloop
 	ofGetMainLoop()->addWindow( mainWindow );
 
 	{
 		ofVkWindowSettings settings;
-		settings.setVkVersion( 1, 0, 39 );
+		settings.setVkVersion( 1, 0, 46 );
 		settings.numSwapchainImages = 3;
 		settings.numVirtualFrames = 3;
 		settings.presentMode = ::vk::PresentModeKHR::eMailbox;
 
+		// Only load debug layers if app is compiled in Debug mode
 #ifdef NDEBUG
 		settings.useDebugLayers = false;
 #else
